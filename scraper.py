5fe93b66c071828f31ff7fe73e76eabbf3e27d5b
Kyle Schick <kschick@umich.edu>
2016-04-07 18:42:32 -0400

    Basic scraping working

--- scraper.py
+++ scraper.py
@@ -1,4 +1,9 @@
 import argparse
+from bs4 import BeautifulSoup
+import urllib2
+
+BASE_URL = 'https://www.reddit.com/r/'
+
 
 def main():
 	parser = argparse.ArgumentParser(description = 'Download pictures from the specified subreddit')
@@ -6,21 +11,27 @@ def main():
 	parser.add_argument('-n', dest='numPics', type=int, nargs=1, default=10,
 		                help='Number of pictures to download. Default is 10.')
 	parser.add_argument('-s', dest='sortedBy', nargs=1, default='top',
-		                choices=['top', 'new', 'hot', 'rising','controversial', 'gilded'],
+		                choices=['top', 'new', 'hot', 'rising','controversial'],
 		                help='Method to choose pictures by. Default is \'top\'.')
-	parser.add_argument('-p', dest='period', nargs=1, default='today',
-		                choices=['hour', 'today', 'week', 'month', 'year', 'alltime'],
-		                help='Time period to choose pictures by. Default is \'today\'.')
+	parser.add_argument('-p', dest='period', nargs=1, default='day',
+		                choices=['hour', 'day', 'week', 'month', 'year', 'all'],
+		                help='Time period to choose pictures by. Default is \'day\'.')
 	parser.add_argument('-fn', dest='folderName', nargs=1, default='',
 		                help='Name of the folder to output to. Wrap multi-word names with '
 		                'quotations. Default is \'[SubredditName] (Date)\'.')
 	args = parser.parse_args()
-	scrapeSubreddit(subredditName=args.subreddit, sortedBy=args.sortedBy[0],
-		            period=args.period[0], folderName=args.folderName[0])
+	scrapeSubreddit(subredditName=args.subreddit, numPics=args.numPics, sortedBy=args.sortedBy,
+		            period=args.period, folderName=args.folderName[0])
 
-def scrapeSubreddit(subredditName, sortedBy, period, folderName):
-	print subredditName
-	
+def scrapeSubreddit(subredditName, numPics, sortedBy, period, folderName):
+	url = BASE_URL + subredditName + '/' + sortedBy + '/?sort=' + sortedBy + '&t=' + period
+	print url
+	opener = urllib2.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+	html = opener.open(url).read()
+	soup = BeautifulSoup(html, "lxml")
+	titles = soup.findAll("a", "title may-blank ")
+	print titles[0].text
 
 if __name__ == '__main__':
 	main()
